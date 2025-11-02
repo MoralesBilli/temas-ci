120b99541664b899cc833138b69f8a81e6784d2c
billi <billi@BILLIJO>
2025-11-01 17:18:08 -0700

    cambiar-contraseña

--- Backend/Rutas/Inicio_Sesion.py
+++ Backend/Rutas/Inicio_Sesion.py
@@ -30,3 +30,31 @@ def login():
         
     except Exception as e:
         return jsonify({'error', 'Error al iniciar sesion'}),500
+
+
+@Inicio_sesion_bp.route('/api/cambiar-contraseña',methods=['PUT'])
+def cambiar_contrasena(id):
+
+    try:
+        data = request.get_json()
+        nueva_contrasena = data.get('nueva_contrasena')
+        contrasena_actual = data.get('contrasena_actual')
+        if not nueva_contrasena or not contrasena_actual :
+            return jsonify({'error':'La contraseña actual y nueva contraseña son requeridas'}), 400
+        
+        if len(nueva_contrasena) < 8:
+            return jsonify({'error': 'La nueva contraseña debe tener al menos 8 caracteres'}), 400
+        if not any(c.isupper() for c in nueva_contrasena):
+            return jsonify({'error': 'La nueva contraseña debe tener al menos una letra mayúscula'}), 400
+        if not any(c.isdigit() for c in nueva_contrasena):
+            return jsonify({'error': 'La nueva contraseña debe tener al menos un número'}), 400
+        if contrasena_actual == nueva_contrasena:
+            return jsonify({'error': 'La nueva contraseña debe ser distinta a la contraseña acctual'}), 400
+        
+        usuario = Inicio_Sesion.query.get(id)
+        if not usuario:
+            return jsonify({'error':'Usuario no encontrado'}),401
+        
+    except Exception as e:
+        return jsonify({'error': 'Error interno del servidor'}), 500
+    
