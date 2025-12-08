04ace58f24aebd88975720abb23e273e94b75476
billi <billi@BILLIJO>
2025-12-07 19:34:33 -0800

    07/12/2025-21

--- frontend/src/app/calidad/components/control/control.spec.ts
+++ frontend/src/app/calidad/components/control/control.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { Control } from './control';
 
 describe('Control', () => {
@@ -8,9 +8,13 @@ describe('Control', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Control]
+      imports: [
+        Control,                 
+        HttpClientTestingModule  
+      ],
     })
     .compileComponents();
+
 
     fixture = TestBed.createComponent(Control<any>);
     component = fixture.componentInstance;
